e6760fd8064bb9ede46dfabc7263337f211587b1
Sean Newman <seangnewman@msn.com>
2018-07-08 19:26:11 -0700

    Add project files.

---
+++ 70_483_ToolKit_For_Certification/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70_483_ToolKit_For_Certification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
