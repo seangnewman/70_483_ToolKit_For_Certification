9942a678d573c3b89916f52ffa35e6f6b93e9357
Sean Newman <seangnewman@msn.com>
2018-07-27 21:05:01 -0700

    Task parallel library

---
+++ Chap7_AsyncWPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chap7_AsyncWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            GetDataAsync();
+        }
+
+        private void GetData()
+        {
+            lblResult.Content = ReadDataFromIO();
+            lblResult2.Content = ReadDataFromIO();  
+        }
+
+        private async Task GetDataAsync()
+        {
+            var task1 = ReadDataFromIOAsync();
+            var task2 = ReadDataFromIOAsync();
+
+            // Here we can do more processing
+            // that doesn't need the data from the previous calls
+
+            //Now we need the data so we have to wait
+            await Task.WhenAll(task1, task2);
+
+            //NOw we have data to show
+            lblResult.Content = task1.Result;
+            lblResult2.Content = task2.Result;
+        }
+      
+        public static Task<double> ReadDataFromIOAsync()
+        {
+            return Task.Run(new Func<double>(ReadDataFromIO));
+        }
+
+        static double ReadDataFromIO()
+        {
+            // We are simulating an I/O by putting the current thread to sleep
+            Thread.Sleep(1000);
+            return 1000d;
+
+        }
+
+
+
+         
+    }
+}
---
+++ Chap7_Async_Example/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chap7_Async_Example
+{
+    class Program
+    {
+        const int NUMBER_OF_ITERATIONS = 32;
+
+        static void Main(string[] args)
+        {
+            //We are using Stopwatch to time the code
+            Stopwatch sw = Stopwatch.StartNew();
+
+        
+            ReadDataFromIOAsync();
+
+            //Print the time it took to run the application
+            Console.WriteLine("We're done in {0}ms!", sw.ElapsedMilliseconds);
+            Console.ReadKey();
+
+
+        }
+
+        
+
+        static double ReadDataFromIO()
+        {
+            // We are simulating an I/O by putting the current thread to sleep
+            Thread.Sleep(50);
+            return 10d;
+
+        }
+
+        public static Task<double> ReadDataFromIOAsync()
+        {
+            return Task.Run(new Func<double> (ReadDataFromIO));
+        }
+
+        static double DoIntensiveCalculations()
+        {
+            // We are simulating intensive calculations
+            // by performing nonsense divisions
+            double result = 10000d;
+            var maxValue = Int32.MaxValue >> 4;
+
+            for (int i = 1; i < maxValue; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result /= i;
+                }
+                else
+                {
+                    result *= i;
+                }
+
+                //Console.WriteLine("The result is {0}", i);
+            }
+            return result + 10d;
+        }
+
+
+
+    }
+}
