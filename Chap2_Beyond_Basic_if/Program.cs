f45ae55e875d0c9aeb07415a5fb422120265d130
Sean Newman <seangnewman@msn.com>
2018-08-15 04:56:44 -0700

    Chapter 2 Basics

---
+++ Chap2_Beyond_Basic_if/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap2_Beyond_Basic_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition1;
+            bool condition2;
+            bool condition3;
+
+            // single if statement
+            condition1 = true;
+
+            if (condition1)
+            {
+                Console.WriteLine("This statement prints if condition is true");
+            }
+            Console.WriteLine("This statement executes regadless of condition");
+            Console.WriteLine();
+
+            //nested if statement
+            condition1 = true;
+            condition2 = true;
+
+            if (condition1)
+            {
+                if (condition2)
+                {
+                    Console.WriteLine("This only prints if both conditions are true");
+                }
+            }
+
+            Console.WriteLine();
+            // if statement with logical operator
+            condition1 = true;
+            condition2 = true;
+
+            if( condition1 && condition2)
+            {
+                Console.WriteLine("This only prints if both conditions are true.");
+            }
+            Console.WriteLine();
+
+            // if-else statement
+            condition1 = true;
+            if (condition1)
+            {
+                Console.WriteLine("This statement prints if statement is true");
+            }
+            else
+            {
+                Console.WriteLine("This statement prints if statement is false");
+            }
+
+            Console.WriteLine("This statement prints regardless of condition");
+            Console.WriteLine();
+
+            // if-else statement
+            condition1 = true;
+            condition2 = false;
+            condition3 = false;
+
+            if (condition1)
+            {
+                Console.WriteLine("This statement prints if condition1 is true");
+            }
+            else if (condition2)
+            {
+                Console.WriteLine("This statement prints if condition2 is true");
+            }else if (condition3)
+            {
+                Console.WriteLine("This statement prints if condition3 is true");
+            }
+            else
+            {
+                Console.WriteLine("This statment prints if previous conditions are false");
+            }
+            Console.WriteLine("This statement executes regardless of condition.");
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chap2_average_grades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap2_average_grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach loop to average grades in array
+            //setup an integer array and assign some values
+            int[] arrGrades = new int[] { 78, 89, 90, 76, 98, 65 };
+
+            //create three variables to hold the sum, number of grades, and average
+            int total = 0;
+            int gradeCount = 0;
+            double average = 0.0;
+
+            // loop to iterate over each integer value in array
+            // foreach doesn't need to know the size initially as it is determined
+            // at the time the array is accessed
+            foreach( int grade in arrGrades)
+            {
+                total = total + grade;      // add each grade value to total
+                gradeCount++;               // increment counter to use in average
+            }
+
+            average = total / gradeCount;   // Calculate average of grades
+            Console.WriteLine(average);
+
+            Console.ReadKey();
+        }
+    }
+}
